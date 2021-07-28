7782fddcd05296cacd16e26c4df7377686dc8e7d
Dofas <dofafoga@gmail.com>
2021-07-29 00:51:49 +0300

    Can input value from buttons and clean the calculator

--- src/App.js
+++ src/App.js
@@ -1,43 +1,111 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
 
-  const [result, setResult] = useState();
-  const [firstNumber, setFirstNumber] = useState();
-  const [secondNumber, setSecondNumber] = useState();
+  const handleNumber = (e) => {
+    setInputValue(e.target.value);
+  };
 
+  // console.log(parseFloat(inputValue))
   return (
     <div className="App">
-      <input className="numbers" type="text"/>
+      <input
+        className="numbers"
+        type="text"
+        onChange={handleNumber}
+        value={inputValue}
+      />
       <div className="buttons">
         <div className="buttons-row">
-          <button>A/C</button>
+          <button onClick ={() => {setInputValue("")}}>A/C</button>
           <button>+/-</button>
           <button>%</button>
           <button>/</button>
         </div>
         <div className="buttons-row">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
+          <button
+            onClick={() => {
+              setInputValue(inputValue.concat("7"));
+            }}
+          >
+            7
+          </button>
+          <button
+            onClick={() => {
+              setInputValue(inputValue.concat("8"));
+            }}
+          >
+            8
+          </button>
+          <button
+            onClick={() => {
+              setInputValue(inputValue.concat("9"));
+            }}
+          >
+            9
+          </button>
           <button>*</button>
         </div>
         <div className="buttons-row">
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
+          <button
+            onClick={() => {
+              setInputValue(inputValue.concat("4"));
+            }}
+          >
+            4
+          </button>
+          <button
+            onClick={() => {
+              setInputValue(inputValue.concat("5"));
+            }}
+          >
+            5
+          </button>
+          <button
+            onClick={() => {
+              setInputValue(inputValue.concat("6"));
+            }}
+          >
+            6
+          </button>
           <button>-</button>
         </div>
         <div className="buttons-row">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+          <button
+            onClick={() => {
+              setInputValue(inputValue.concat("1"));
+            }}
+          >
+            1
+          </button>
+          <button
+            onClick={() => {
+              setInputValue(inputValue.concat("2"));
+            }}
+          >
+            2
+          </button>
+          <button
+            onClick={() => {
+              setInputValue(inputValue.concat("3"));
+            }}
+          >
+            3
+          </button>
           <button>+</button>
         </div>
         <div className="buttons-row">
-          <button className="nullButton">0</button>
-          <button>,</button>
+          <button
+            className="nullButton"
+            onClick={() => {
+              setInputValue(inputValue.concat("0"));
+            }}
+          >
+            0
+          </button>
+          <button onClick={ () => {setInputValue(inputValue.concat('.'))}}>.</button>
           <button>=</button>
         </div>
       </div>
